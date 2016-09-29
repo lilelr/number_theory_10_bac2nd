bcbafcbe7f776fbc2a0712df7236e2bd4e6ccb97
lilelr <lilelr@163.com>
2016-09-29 11:28:58 +0800

    Initial Commit

---
+++ number_theory_10/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  number_theory_10
+//
+//  Created by YuXiao on 9/29/16.
+//  Copyright © 2016 YuXiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
