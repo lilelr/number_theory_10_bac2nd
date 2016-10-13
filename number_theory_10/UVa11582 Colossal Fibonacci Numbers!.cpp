802c34582fb86fea8f031cf3817b8ef06814808c
lilelr <lilelr@163.com>
2016-10-13 11:29:25 +0800

    n

--- number_theory_10/UVa11582 Colossal Fibonacci Numbers!.cpp
+++ number_theory_10/UVa11582 Colossal Fibonacci Numbers!.cpp
@@ -1,56 +1,56 @@
-// UVa11582 Colossal Fibonacci Numbers!
-// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=850&page=show_problem&problem=4510
-// 大数求余 + 数列规律探索
-// Rujia Liu
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-using namespace std;
-
-const int maxn = 1000 + 5;
-typedef unsigned long long ULL;
-
-// 对于每个n, 计算f[n][i] 的值
-int f[maxn][maxn * 6];
-// 对于每个 n, 计算它的period[n] 的周期
-int period[maxn];
-
-// a^b %n
-int pow_mod(ULL a, ULL b, int n) {
-  if (!b)
-    return 1;
-  int k = pow_mod(a, b / 2, n);
-  k = k * k % n;
-  if (b % 2)
-    k = k * a % n;
-  return k;
-}
-
-int solve(ULL a, ULL b, int n) {
-  if (a == 0 || n == 1)
-    return 0; // attention!
-  int p = pow_mod(a % period[n], b, period[n]);
-  return f[n][p]; // p 为周期中的位置，返回那个位置的元素即为答案
-}
-
-int main() {
-  for (int n = 2; n <= 1000; n++) {
-    f[n][0] = 0;
-    f[n][1] = 1;
-    for (int i = 2;; i++) {
-      f[n][i] = (f[n][i - 1] + f[n][i - 2]) % n;
-      if (f[n][i - 1] == 0 && f[n][i] == 1) {
-        period[n] = i - 1; // 重复，周期为i-1.
-        break;
-      }
-    }
-  }
-  ULL a, b;
-  int n, T;
-  cin >> T;
-  while (T--) {
-    cin >> a >> b >> n;
-    cout << solve(a, b, n) << "\n";
-  }
-  return 0;
-}
+//// UVa11582 Colossal Fibonacci Numbers!
+//// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=850&page=show_problem&problem=4510
+//// 大数求余 + 数列规律探索
+//// Rujia Liu
+//#include <cstdio>
+//#include <cstring>
+//#include <iostream>
+//using namespace std;
+//
+//const int maxn = 1000 + 5;
+//typedef unsigned long long ULL;
+//
+//// 对于每个n, 计算f[n][i] 的值
+//int f[maxn][maxn * 6];
+//// 对于每个 n, 计算它的period[n] 的周期
+//int period[maxn];
+//
+//// a^b %n
+//int pow_mod(ULL a, ULL b, int n) {
+//  if (!b)
+//    return 1;
+//  int k = pow_mod(a, b / 2, n);
+//  k = k * k % n;
+//  if (b % 2)
+//    k = k * a % n;
+//  return k;
+//}
+//
+//int solve(ULL a, ULL b, int n) {
+//  if (a == 0 || n == 1)
+//    return 0; // attention!
+//  int p = pow_mod(a % period[n], b, period[n]);
+//  return f[n][p]; // p 为周期中的位置，返回那个位置的元素即为答案
+//}
+//
+//int main() {
+//  for (int n = 2; n <= 1000; n++) {
+//    f[n][0] = 0;
+//    f[n][1] = 1;
+//    for (int i = 2;; i++) {
+//      f[n][i] = (f[n][i - 1] + f[n][i - 2]) % n;
+//      if (f[n][i - 1] == 0 && f[n][i] == 1) {
+//        period[n] = i - 1; // 重复，周期为i-1.
+//        break;
+//      }
+//    }
+//  }
+//  ULL a, b;
+//  int n, T;
+//  cin >> T;
+//  while (T--) {
+//    cin >> a >> b >> n;
+//    cout << solve(a, b, n) << "\n";
+//  }
+//  return 0;
+//}
---
+++ number_theory_10/UVa12169 Disgruntled Judge (slow solution).cpp
@@ -0,0 +1,45 @@
+//
+//  UVa12169 Disgruntled Judge (slow solution).cpp
+//  number_theory_10
+//
+//  Created by YuXiao on 10/13/16.
+//  Copyright © 2016 YuXiao. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+const int maxn = 100 * 2 + 5;
+const int M = 10001;
+int T, x[maxn];
+
+void solve() {
+  for (int a = 0; a < M; a++) {
+    for (int b = 0; b < M; b++) {
+      bool ok = true;
+      for (int i = 2; i <= T * 2; i += 2) {
+        x[i] = (a * x[i - 1] + b) % M;
+        if (i + 1 <= T * 2 && x[i + 1] != (a * x[i] + b) % M) {
+          ok = false;
+          break;
+        }
+      }
+      if (ok) {
+        return;
+      }
+    }
+  }
+}
+
+int main() {
+  while (cin >> T) {
+    for (int i = 1; i <= T * 2 - 1; i += 2) {
+      cin >> x[i];
+    }
+    solve();
+    for (int i = 2; i <= T * 2; i += 2) {
+      cout << x[i] << "\n";
+    }
+  }
+  return 0;
+}
